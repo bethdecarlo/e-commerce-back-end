d5de87b15bdb044f10c08e94c13bb321b1ff9d78
Beth DeCarlo <110702442+bethdecarlo@users.noreply.github.com>
2022-11-05 21:23:28 -0400

    added 2 coulmns to Category.js

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,25 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// I added two columns:
+//The id column, which will give the category an auto-incremented id.
+//The category_name column which will alow the data to be contained as a string. 
 Category.init(
   {
-    // define columns
+    id: {
+    //properties of the id column
+      type: DataTypes.INTEGER, 
+      allowNull : false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    category_name: {
+      //properties of the category name column
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   },
   {
     sequelize,
